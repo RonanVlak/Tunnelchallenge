bb4b0d73318372ce712a5061dc311672dcc56144
RonanVlak <ronanvlak@gmail.com>
2023-04-10 14:09:26 +0200

    Updated for BMP280 sensor

--- Sensors/Sensors.ino
+++ Sensors/Sensors.ino
@@ -1,11 +1,26 @@
 #include <Wire.h>
+#include <Adafruit_BMP280.h>
+#define BMP280_ADDRESS 0x77
+Adafruit_BMP280 bmp;
+//Installeer Adafruit Unified Sensor library en Adafruit BMP280 en Adafruit BusIO libraries vanaf library manager
 
-int buf[16];
-int16_t val;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Wire.begin();
+  unsigned status;
+  status = bmp.begin(BMP280_ADDRESS);
+  if(!status) {
+    Serial.print("Wrong address, sensorID was 0x");
+    Serial.println(bmp.sensorID(), 16);
+  }
+
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 }
 
 
@@ -15,43 +30,25 @@ void readCO() {
   Serial.println(sensorCO);
 }
 
-float readSensor(uint8_t regAddr) {
-  buf[0] = 0x00;
-    Wire.beginTransmission(0x40 << 1) ;
-    Wire.write(regAddr);
-    Wire.write(1);
-    Wire.endTransmission();
 
-    Wire.requestFrom(0x40 << 1, 2);
-    buf[0] = Wire.read();
-    val = ((uint16_t) buf[0] << 8);
-    val |= buf[1];
-    
-  return val;
-}
 void readTemp() {
-  uint16_t temp;
-  float temperature;
-  temp = readSensor(0xE3);
-  temperature = ((175.72*temp)/65536) - 46.85; //calculate temperature according to datasheet
+  int temperature = bmp.readTemperature();
   Serial.print("TP ");
   Serial.println(temperature);
 }
 
-void readHumid() {
-  uint16_t temp;
-  float humidity;
-  temp = readSensor(0xE5);
-  humidity = ((125*temp)/65536) - 6; //calculate humidity according to datasheet
-  Serial.print("HM ");
-  Serial.println(humidity);
+void readPress() {
+  int pressure = bmp.readPressure();
+  Serial.print("PR ");
+  Serial.println(pressure);
 }
+
 void loop() {
   readCO();
   delay(500);
   readTemp();
   delay(500);
-  readHumid();
+  readPress();
   delay(500);
   }
   
